f8b2573fd65a9619069d3055dd5785f380d78c24
martinkabe <martinkabe@seznam.cz>
2019-02-16 11:38:54 +0100

    Text file separator improvement

--- inst/Loader/Functions.cs
+++ inst/Loader/Functions.cs
@@ -217,41 +217,41 @@ namespace csv_to_sql_loader
                 char[] seps = { '\t', ',', '.', ';', '|', '~', '^', ' '};
                 foreach (char sprt in seps)
                 {
-                    if (sprt == '\t')
+                    if (sprt == sep)
                     {
                         Console.WriteLine("Tab is used as separator.");
                     }
-                    else if (sprt == ',')
+                    else if (sprt == sep)
                     {
                         Console.WriteLine("Comma is used as separator.");
                     }
-                    else if (sprt == '.')
+                    else if (sprt == sep)
                     {
                         Console.WriteLine("Dot is used as separator.");
                     }   
-                    else if (sprt == ';')
+                    else if (sprt == sep)
                     {
                         Console.WriteLine("Semi-colon is used as separator.");
                     }
-                    else if (sprt == '|')
+                    else if (sprt == sep)
                     {
                         Console.WriteLine("Pipe is used as separator.");
                     }
-                    else if (sprt == '~')
+                    else if (sprt == sep)
                     {
                         Console.WriteLine("Tilde is used as separator.");
                     }
-                    else if (sprt == '^')
+                    else if (sprt == sep)
                     {
                         Console.WriteLine("Caret is used as separator.");
                     }
-                    else if (sprt==' ')
+                    else if (sprt== sep)
                     {
                         Console.WriteLine("Space is used as separator.");
                     }
                     else
                     {
-                        Console.WriteLine("[" + sprt + "] is used as separator.");
+                        Console.WriteLine("[" + sep + "] is used as separator.");
                     }
                 }
                 
